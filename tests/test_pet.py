dcd982549231daeafa8faca5ce36975112474386
motorych88 <motorych88@mail.ru>
2025-04-16 21:07:27 +0300

    Сделал 3 ДЗ

--- tests/test_pet.py
+++ tests/test_pet.py
@@ -52,7 +52,37 @@ class TestPet:
                 "status": "available"
             }
             response = requests.post(url=f'{BASE_URL}/pet', json=payload)
+            response_json = response.json()
         with allure.step('Проверка статуса ответа'):
             assert response.status_code == 200, 'Статус код ответа неверный'
+            jsonschema.validate(response_json, PET_SCHEMA)
         with allure.step('Проверка JSON ответа'):
-            jsonschema.validate(response.json(), PET_SCHEMA)
+            assert response_json["id"] == payload["id"]
+            assert response_json["name"] == payload["name"]
+            assert response_json["status"] == payload["status"]
+
+    @allure.title('Добавление нового питомца c полным телом')
+    def test_add_fullpayload_pet(self):
+        with allure.step('Отправка запроса на добавление'):
+            payload = {
+                "id": 10,
+                "name": "doggie",
+                "category": {"id": 1, "name": "Dogs"},
+                "photoUrls": ["string"],
+                "tags": [{"id": 0, "name": "string"}],
+                "status": "available"
+            }
+            response = requests.post(url=f'{BASE_URL}/pet', json=payload)
+            response_json = response.json()
+        with allure.step('Проверка статуса ответа'):
+            assert response.status_code == 200, 'Статус код ответа неверный'
+            jsonschema.validate(response_json, PET_SCHEMA)
+        with allure.step('Проверка JSON ответа'):
+            assert response_json["id"] == payload["id"], 'id тела не совпадает с id ответа'
+            assert response_json["name"] == payload["name"]
+            assert response_json["category"]["id"] == payload["category"]["id"]
+            assert response_json["category"]["name"] == payload["category"]["name"]
+            assert response_json["photoUrls"] == payload["photoUrls"]
+            assert response_json["tags"][0]["id"] == payload["tags"][0]["id"]
+            assert response_json["tags"][0]["name"] == payload["tags"][0]["name"]
+            assert response_json["status"] == payload["status"]
