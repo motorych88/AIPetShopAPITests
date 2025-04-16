265e5df161425a28bd55742b468c8827691da8d8
motorych88 <motorych88@mail.ru>
2025-04-16 17:49:12 +0300

    добавил jsonschema

--- tests/test_pet.py
+++ tests/test_pet.py
@@ -1,7 +1,9 @@
 from http.client import responses
 
 import allure
+import jsonschema
 import requests
+from .schemas.pet_schema import PET_SCHEMA
 
 BASE_URL = 'http://5.181.109.28:9090/api/v3'
 
@@ -40,3 +42,17 @@ class TestPet:
             assert response.status_code == 404, 'Статус код ответа неверный'
         with allure.step('Проверка текста ответа'):
             assert response.text == 'Pet not found', 'Текст ответа неверный'
+
+    @allure.title('Добавление нового питомца')
+    def test_add_pet(self):
+        with allure.step('Отправка запроса на добавление'):
+            payload = {
+                "id": 800,
+                "name": "Jackson",
+                "status": "available"
+            }
+            response = requests.post(url=f'{BASE_URL}/pet', json=payload)
+        with allure.step('Проверка статуса ответа'):
+            assert response.status_code == 200, 'Статус код ответа неверный'
+        with allure.step('Проверка JSON ответа'):
+            jsonschema.validate(response.json(), PET_SCHEMA)
